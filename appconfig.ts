841ec0d32aa56a3747a78c64aaf2d89b44a7b156
Tom Ball (MSR) <tball@microsoft.com>
2024-10-16 09:01:32 -0700

    add AppInterface

--- appconfig.ts
+++ appconfig.ts
@@ -2,4 +2,8 @@ namespace user_interface_base {
     export const font = bitmaps.font8
     export let getIcon: (name: string, nullIfMissing: boolean) => Bitmap  = null
     export let resolveTooltip: (ariaId: string) => string = null
+    export interface AppInterface {
+        pushScene(scene: Scene): void
+        popScene(): void
+    }
 }
