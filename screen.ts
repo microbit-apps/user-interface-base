e1574a51a7f5cf4e4d031eec7b9134ffa07fe98a
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-05-13 09:58:00 +0100

    screen.ts: tryToSend debugging

--- screen.ts
+++ screen.ts
@@ -73,16 +73,20 @@ namespace user_interface_base {
         public static tryToSend(data: Buffer | String) {
             let received = false;
             radio.onReceivedString((s: String) => {
+                basic.showString("R")
                 if (s == "ACK")
                     received = true;
             })
 
+            let i = 0;
             let timePassed = 0;
             while (!received) {
                 if (timePassed % 30 == 0) {
                     if (typeof data == "string") {
                         radio.sendString(data);
                     } else {
+                        basic.showNumber(i)
+                        i++;
                         radio.sendBuffer(data as Buffer);
                     }
                 }
